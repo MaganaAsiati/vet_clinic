581a2b0e84c2c791fb6a13ac5e3cdb63c7fc44c6
MaganaAsiati <assia.magana@gmail.com>
2022-07-04 23:42:41 +0200

    add query which List the name of all animals that are neutered and have less than 3 escape attempts.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT * FROM animals WHERE date_of_birth >='2016-01-01' and date_of_birth<='2019-12-31';
+SELECT name FROM animals WHERE date_of_birth >='2016-01-01' and date_of_birth<='2019-12-31';
+SELECT  name FROM animals WHERE  neutered = 'true' and escape_attempts < 3 ;
