5eea11be2f355ac7f428c31679a6210125cf1b90
MaganaAsiati <assia.magana@gmail.com>
2022-07-05 01:37:21 +0200

    add query which help us to find all animals that are neutered.

--- queries.sql
+++ queries.sql
@@ -5,5 +5,6 @@ SELECT name FROM animals WHERE date_of_birth >='2016-01-01' and date_of_birth<='
 SELECT  name FROM animals WHERE  neutered = 'true' and escape_attempts < 3 ;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu' ;
 SELECT name, escape_attempts FROM animals WHERE  weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name <> 'Gabumon' ; 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
