c8a28f2f685df4d889d2590433d0a9e877347464
MaganaAsiati <assia.magana@gmail.com>
2022-07-04 23:49:03 +0200

    add query which help us to List the date of birth of all animals named either 'Agumon' or 'Pikachu'

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth >='2016-01-01' and date_of_birth<='2019-12-31';
 SELECT  name FROM animals WHERE  neutered = 'true' and escape_attempts < 3 ;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu' ;
