dc211f15d9a32cdd9aeb8d34190961d61d19af87
MaganaAsiati <assia.magana@gmail.com>
2022-07-04 23:15:59 +0200

    add query whitch List the name of all animals born between 2016 and 2019

--- queries.sql
+++ queries.sql
@@ -1,3 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE date_of_birth >='2016-01-01' and date_of_birth<='2019-12-31';
