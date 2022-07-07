5c646148966c9326f532a447b54e2616bdc83455
MaganaAsiati <assia.magana@gmail.com>
2022-07-07 02:46:31 +0200

    add relation between tables and drop species

--- schema.sql
+++ schema.sql
@@ -22,4 +22,12 @@ CREATE TABLE IF NOT EXISTS species(
     name VARCHAR(100)
 );
 
+ALTER TABLE animals
+DROP COLUMN species;
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD FOREIGN KEY (species_id) REFERENCES species(id),
+ADD COLUMN owner_id INT,
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
 
