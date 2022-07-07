4d54011a462a3929053418fab5af3bbaa288e278
MaganaAsiati <assia.magana@gmail.com>
2022-07-07 03:00:28 +0200

    add query zhich help us to List all animals owned by Dean Winchester that haven't tried to escape.

--- queries.sql
+++ queries.sql
@@ -89,3 +89,12 @@ ON O.id = A.owner_id
 WHERE O.full_name = 'Jennifer Orwell';
 
 
+SELECT A.name, O.full_name
+FROM animals A
+JOIN species S
+ON S.id = A.species_id
+JOIN owners O
+ON O.id = A.owner_id
+WHERE O.full_name = 'Dean Winchester' AND A.escape_attempts = 0;
+
+
