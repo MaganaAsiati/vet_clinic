78189b9a5edb43d496cdd18a49032844fa630962
MaganaAsiati <assia.magana@gmail.com>
2022-07-12 14:14:13 +0200

    add query to Add patients table to clinic database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -2,7 +2,14 @@
 
 CREATE DATABASE clinic;
 
+/* Add patients table to clinic database. */
 
+CREATE TABLE IF NOT EXISTS patients(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(255),
+ date_of_birth DATE
+
+);
 
 
 
