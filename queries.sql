85d06be5651ff442a353e5ccbeef650b737fdcdd
Assad Isah <nottherealalanturing@gmail.com>
2022-07-12 09:37:34 +0100

    queries: normalize

--- queries.sql
+++ queries.sql
@@ -171,3 +171,8 @@ ORDER BY Animal_Highest_Count DESC LIMIT 1;
         GROUP BY species.name
         ORDER BY species.name DESC 
         LIMIT 1;
+
+
+        EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+        EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+        EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
